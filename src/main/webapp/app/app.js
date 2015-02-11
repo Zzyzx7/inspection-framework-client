20ebdb78ae910dfd22c968d8481003f8dcfc0563
Zzyzx7 <dominicwalter@web.de>
2015-02-11 18:54:40 +0100

    update ng-view

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -5,16 +5,16 @@ var inspectionFrameworkApp = angular.module('inspectionFrameworkApp', [
 inspectionFrameworkApp.config(['$routeProvider', '$locationProvider',
 		                    function($routeProvider, $locationProvider) {
 		                      $routeProvider.
-		                        when('/inspectionObjects', {
+		                        when('/objects', {
 		                          templateUrl: 'views/inspection-object-list.html',
 		                          controller: 'InspectionObjectListCtrl'
 		                        }).
-		                        when('/inspectionObjects/:inspectionObjectId', {
+		                        when('/objects/:id', {
 		                          templateUrl: 'views/inspection-object-detail.html',
 		                          controller: 'InspectionObjectDetailCtrl'
 		                        }).
 		                        otherwise({
-		                          redirectTo: '/inspectionObjects'
+		                          redirectTo: '/objects'
 		                        });
 		                    }]);
 
--- src/main/webapp/app/controllers.js
+++ src/main/webapp/app/controllers.js
@@ -1,14 +1,15 @@
 var inspectionObjectControllers = angular.module('inspectionObjectControllers', []);
 
-inspectionObjectControllers.controller('InspectionObjectListCtrl', ['$scope', '$http', 
-  function ($scope, $http) {
-	  $http.get(REST_BACKEND_URL + '/inspectionobject').success(function(data) {
-		  $scope.inspectionObjects = data
-	  })
-	  $scope.orderProp = 'objectName';
-	}]);
+inspectionObjectControllers.controller('InspectionObjectListCtrl', ['$scope', '$http',
+                                                 function ($scope, $http) {
+                                                   $http.get('https://inspection-framework.herokuapp.com/assignment').success(function(data) {
+                                                     $scope.objects = data;
+                                                   });
+
+                                                   //$scope.orderProp = 'age';
+                                                 }]);
 
 inspectionObjectControllers.controller('InspectionObjectDetailCtrl', ['$scope', '$routeParams',
-                                                   function($scope, $routeParams) {
-                                                     $scope.inspectionObjectId = $routeParams.inspectionObjectId;
-                                                   }]);
+                                                 function($scope, $routeParams) {
+                                                   $scope.id = $routeParams.id;
+                                                 }]);
