d1b95f30ac06d75723e1cc8311a1fb0707c0ffa5
Tobias Höhmann <tobiashoehmann@googlemail.com>
2015-02-13 08:19:41 +0100

    initial changes for post / delete operations

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,17 +1,26 @@
 var inspectionFrameworkApp = angular.module('inspectionFrameworkApp', [
 	                                                                 'ngRoute',
-	                                                                 'inspectionObjectControllers'
+	                                                                 'inspectionObjectControllers',
+	                                                                 'assignmentControllers'
 	                                                               ]);
 inspectionFrameworkApp.config(['$routeProvider', '$locationProvider',
 		                    function($routeProvider, $locationProvider) {
 		                      $routeProvider.
+		                        when('/inspectionobject', {
+		                          templateUrl: 'views/inspection-object-list.html',
+		                          controller: 'InspectionObjectListCtrl'
+		                        }).
+		                        when('/inspectionobject/:id', {
+		                          templateUrl: 'views/inspection-object-detail.html',
+		                          controller: 'InspectionObjectDetailCtrl'
+		                        }).
 		                        when('/assignments', {
 		                          templateUrl: 'views/list-assignments.html',
-		                          controller: 'InspectionObjectListCtrl'
+		                          controller: 'AssignmentListCtrl'
 		                        }).
 		                        when('/assignments/:id', {
 		                          templateUrl: 'views/list-assignments-detail.html',
-		                          controller: 'InspectionObjectDetailCtrl'
+		                          controller: 'AssignmentDetailCtrl'
 		                        }).
 		                        otherwise({
 		                          redirectTo: '/'
--- src/main/webapp/app/controllers.js
+++ src/main/webapp/app/controllers.js
@@ -2,6 +2,25 @@ var inspectionObjectControllers = angular.module('inspectionObjectControllers', 
 
 inspectionObjectControllers.controller('InspectionObjectListCtrl', ['$scope', '$http',
                                                  function ($scope, $http) {
+                                                   $http.get('https://inspection-framework.herokuapp.com/inspectionobject').success(function(data) {
+                                                     $scope.inspectionobjects = data;
+                                                   });
+
+                                                   $scope.orderProp = 'endDate';
+                                                 }]);
+
+inspectionObjectControllers.controller('InspectionObjectDetailCtrl', ['$scope', '$routeParams', '$http',
+                                                 function($scope, $routeParams, $http) {
+	$http.get('https://inspection-framework.herokuapp.com/inspectionobject/' + $routeParams.id).success(function(data) {
+	      $scope.object = data;
+	    });
+                                                 }]);
+
+
+var assignmentControllers = angular.module('assignmentControllers', []);
+
+assignmentControllers.controller('AssignmentListCtrl', ['$scope', '$http',
+                                                 function ($scope, $http) {
                                                    $http.get('https://inspection-framework.herokuapp.com/assignment').success(function(data) {
                                                      $scope.objects = data;
                                                    });
@@ -9,7 +28,7 @@ inspectionObjectControllers.controller('InspectionObjectListCtrl', ['$scope', '$
                                                    $scope.orderProp = 'endDate';
                                                  }]);
 
-inspectionObjectControllers.controller('InspectionObjectDetailCtrl', ['$scope', '$routeParams', '$http',
+assignmentControllers.controller('AssignmentDetailCtrl', ['$scope', '$routeParams', '$http',
                                                  function($scope, $routeParams, $http) {
 	$http.get('https://inspection-framework.herokuapp.com/assignment/' + $routeParams.id).success(function(data) {
 	      $scope.object = data;
